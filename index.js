ee9318f0c09aa3f189df00cc90e8db40452479e3
M. Alif Raher <alifraher@gmail.com>
2018-07-25 20:59:55 +0700

    Decrease code line from 200+ to 143 line

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const reset = () => {
     result = 0;
     operator = "";
     number = "";
-    document.getElementById("calculate").value = "";
+    calculate.value = "";
 }
 
 const disableButton = (id = "add", isDisable = false) => {
@@ -30,64 +30,19 @@ const disableOperatorButton = (value) => {
     disableButton("equal", value);
 }
 
-function addNumber(num) {
-    document.getElementById("calculate").value += num;
+const addNumber = (num) => {
+    calculate.value += num;
     number += num;
     disableOperatorButton(false);
 }
 
-function disableKeyboard() {
-    document.onkeydown = function (e) {
-        return false;
-    }
+const clickNumberButton = (num, num1) => {
+    document.getElementById(num).addEventListener("click", function () {
+        addNumber(num1);
+    });
 }
 
-//------------------------------------------------------------------
-//Disable Operator Button
-disableOperatorButton(true);
-disableKeyboard();
-
-//------------------------------------------------------------------
-// Implement DOM on Number Button
-
-document.getElementById("1").addEventListener("click", function () {
-    addNumber(1);
-});
-document.getElementById("2").addEventListener("click", function () {
-    addNumber(2);
-});
-document.getElementById("3").addEventListener("click", function () {
-    addNumber(3);
-});
-document.getElementById("4").addEventListener("click", function () {
-    addNumber(4);
-});
-document.getElementById("5").addEventListener("click", function () {
-    addNumber(5);
-});
-document.getElementById("6").addEventListener("click", function () {
-    addNumber(6);
-});
-document.getElementById("7").addEventListener("click", function () {
-    addNumber(7);
-});
-document.getElementById("8").addEventListener("click", function () {
-    addNumber(8);
-});
-document.getElementById("9").addEventListener("click", function () {
-    addNumber(9);
-});
-document.getElementById("0").addEventListener("click", function () {
-    addNumber(0);
-});;
-document.getElementById("point").addEventListener("click", function () {
-    addNumber(".");
-});
-
-//------------------------------------------------------------------
-// Implement DOM on Math Operator
-
-document.getElementById("add").addEventListener("click", function () {
+const checkOperator = () => {
     if (temp1 == 0) {
         result += parseFloat(number);
         temp1 = 1;
@@ -104,125 +59,76 @@ document.getElementById("add").addEventListener("click", function () {
             result %= parseFloat(number);
         }
     }
+}
 
-    document.getElementById("calculate").value += "+";
+const disableKeyboard = () => document.onkeydown = (e) => false;
+
+//------------------------------------------------------------------
+//Disable Operator Button
+disableOperatorButton(true);
+
+//------------------------------------------------------------------
+// Implement DOM on Number Button
+
+clickNumberButton("1", 1);
+clickNumberButton("2", 2);
+clickNumberButton("3", 3);
+clickNumberButton("4", 4);
+clickNumberButton("5", 5);
+clickNumberButton("6", 6);
+clickNumberButton("7", 7);
+clickNumberButton("8", 8);
+clickNumberButton("9", 9);
+clickNumberButton("0", 0);
+clickNumberButton("point", ".");
+
+//------------------------------------------------------------------
+// Implement DOM on Math Operator
+
+add.addEventListener("click", function () {
+    checkOperator();
+    calculate.value += "+";
     temp = "+"
     number = "";
     disableOperatorButton(true);
     console.log(result);
 });
-document.getElementById("substract").addEventListener("click", function () {
-    if (temp1 == 0) {
-        result += parseFloat(number);
-        temp1 = 1;
-    } else {
-        if (temp == "+") {
-            result += parseFloat(number);
-        } else if (temp == "-") {
-            result -= parseFloat(number);
-        } else if (temp == "x") {
-            result *= parseFloat(number);
-        } else if (temp == ":") {
-            result /= parseFloat(number);
-        } else if (temp == "mod") {
-            result %= parseFloat(number);
-        }
-    }
-
-    document.getElementById("calculate").value += "-";
+substract.addEventListener("click", function () {
+    checkOperator();
+    calculate.value += "-";
     temp = "-"
     number = "";
     disableOperatorButton(true);
     console.log(result);
 });
-document.getElementById("multiply").addEventListener("click", function () {
-    if (temp1 == 0) {
-        result += parseFloat(number);
-        temp1 = 1;
-    } else {
-        if (temp == "+") {
-            result += parseFloat(number);
-        } else if (temp == "-") {
-            result -= parseFloat(number);
-        } else if (temp == "x") {
-            result *= parseFloat(number);
-        } else if (temp == ":") {
-            result /= parseFloat(number);
-        } else if (temp == "mod") {
-            result %= parseFloat(number);
-        }
-    }
-
-    document.getElementById("calculate").value += "x";
+multiply.addEventListener("click", function () {
+    checkOperator();
+    calculate.value += "x";
     temp = "x"
     number = "";
     disableOperatorButton(true);
     console.log(result);
 });
-document.getElementById("divide").addEventListener("click", function () {
-    if (temp1 == 0) {
-        result += parseFloat(number);
-        temp1 = 1;
-    } else {
-        if (temp == "+") {
-            result += parseFloat(number);
-        } else if (temp == "-") {
-            result -= parseFloat(number);
-        } else if (temp == "x") {
-            result *= parseFloat(number);
-        } else if (temp == ":") {
-            result /= parseFloat(number);
-        } else if (temp == "mod") {
-            result %= parseFloat(number);
-        }
-    }
-
-    document.getElementById("calculate").value += ":";
+divide.addEventListener("click", function () {
+    checkOperator();
+    calculate.value += ":";
     temp = ":"
     number = "";
     disableOperatorButton(true);
     console.log(result);
 });
-document.getElementById("modulo").addEventListener("click", function () {
-    if (temp1 == 0) {
-        result += parseFloat(number);
-        temp1 = 1;
-    } else {
-        if (temp == "+") {
-            result += parseFloat(number);
-        } else if (temp == "-") {
-            result -= parseFloat(number);
-        } else if (temp == "x") {
-            result *= parseFloat(number);
-        } else if (temp == ":") {
-            result /= parseFloat(number);
-        } else if (temp == "mod") {
-            result %= parseFloat(number);
-        }
-    }
-
-    document.getElementById("calculate").value += "mod";
+modulo.addEventListener("click", function () {
+    checkOperator();
+    calculate.value += "mod";
     temp = "mod"
     number = "";
     disableOperatorButton(true);
     console.log(result);
 });
-document.getElementById("equal").addEventListener("click", function () {
-    if (number != "") {
-        if (temp == "+") {
-            result += parseFloat(number);
-        } else if (temp == "-") {
-            result -= parseFloat(number);
-        } else if (temp == "x") {
-            result *= parseFloat(number);
-        } else if (temp == ":") {
-            result /= parseFloat(number);
-        } else if (temp == "mod") {
-            result %= parseFloat(number);
-        }
-    }
-    document.getElementById("result").innerHTML = document.getElementById("calculate").value + " = " + result;
-    document.getElementById("calculate").value = "";
+equal.addEventListener("click", function () {
+    checkOperator();
+    output.innerHTML = calculate.value + " = " + result;
+    calculate.value = "";
     console.log(result);
     reset();
     disableOperatorButton(true);
@@ -231,7 +137,7 @@ document.getElementById("equal").addEventListener("click", function () {
 //------------------------------------------------------------------
 // Implement DOM on Clear Button
 
-document.getElementById("clear").addEventListener("click", function () {
+clear.addEventListener("click", function () {
     reset();
     disableOperatorButton(true);
 });
