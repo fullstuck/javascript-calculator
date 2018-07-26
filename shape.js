e940c863f5530c21edfc875dbff8bfddb4f0a1c2
M. Alif Raher <alifraher@gmail.com>
2018-07-26 07:02:39 +0700

    Add circle shape

--- shape.js
+++ shape.js
@@ -11,10 +11,10 @@ const rectangleArea = (length, width) => length * width;
 const rectanglePerimeter = (length, width) => (length * 2) + (width * 2);
 const circleArea = (phi, radius) => phi * (radius * radius);
 const circleCircumference = (phi, radius) => 2 * phi * radius;
-const cubeAreaFunc = (side) => 6 * (length * length);
-const cubeVolumeFunc = (side) => length * length * length;
-const tubeAreaFunc = (phi, radius, height) => 2 * phi * radius * (height + radius);
-const tubeVolumeFunc = (phi, radius, height) => phi * (radius * radius) * height;
+const cubeArea = (side) => 6 * (side * side);
+const cubeVolume = (side) => side * side * side;
+const cylinderArea = (phi, radius, height) => 2 * phi * radius * (height + radius);
+const cylinderVolume = (phi, radius, height) => phi * (radius * radius) * height;
 
 combobox.addEventListener("click", function () {
     var value = combobox.value;
@@ -22,10 +22,19 @@ combobox.addEventListener("click", function () {
         label2.style.display = "inline";
         textbox2.style.display = "inline";
         label1.innerHTML = "Length";
+        perimeter.innerHTML = "Calculate Perimeter";
     } else if (value == "square") {
         label1.innerHTML = "Side";
         label2.style.display = "none";
         textbox2.style.display = "none";
+        perimeter.innerHTML = "Calculate Perimeter";
+    } else if (value == "circle") {
+        label1.innerHTML = "radius";
+        label2.style.display = "none";
+        textbox2.style.display = "none";
+        perimeter.innerHTML = "Calculate Perimeter";
+    } else {
+        alert("Choose the shape first!");
     }
 });
 
@@ -36,14 +45,16 @@ area.addEventListener("click", function () {
         width = textbox2.value;
         total = rectangleArea(length, width);
         answer.value = total;
-        console.log(total);
     } else if (value == "square") {
         side = textbox1.value;
         total = squareArea(side);
         answer.value = total;
-        console.log(total);
+    } else if (value == "circle") {
+        radius = textbox1.value;
+        total = circleArea(phi, radius);
+        answer.value = total;
     } else {
-        alert("Choose the shape first!")
+        alert("Choose the shape first!");
     }
 });
 
@@ -54,12 +65,14 @@ perimeter.addEventListener("click", function () {
         width = textbox2.value;
         total = rectanglePerimeter(length, width);
         answer.value = total;
-        console.log(total);
     } else if (value == "square") {
         side = textbox1.value;
         total = squarePerimeter(side);
         answer.value = total;
-        console.log(total);
+    } else if (value == "circle") {
+        radius = textbox1.value;
+        total = circleCircumference(phi, radius);
+        answer.value = total;
     } else {
         alert("Choose the shape first!")
     }
