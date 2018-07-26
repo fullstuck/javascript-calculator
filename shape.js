75e7a3f9ebd3a0441367ca2dd69d96c4053e4f1f
M. Alif Raher <alifraher@gmail.com>
2018-07-26 07:49:25 +0700

    Use document.getElementById to get id

--- shape.js
+++ shape.js
@@ -1,9 +1,14 @@
+// Declare Variable
+
 let length;
 let width;
 let height;
 let radius;
 let total;
 const phi = Math.PI;
+
+//------------------------------------------------------------------
+// Function
 
 const squareArea = (side) => side * side;
 const squarePerimeter = (side) => side * 4;
@@ -16,92 +21,93 @@ const cubeVolume = (side) => side * side * side;
 const cylinderArea = (phi, radius, height) => 2 * phi * radius * (height + radius);
 const cylinderVolume = (phi, radius, height) => phi * (radius * radius) * height;
 
-combobox.addEventListener("click", function () {
-    var value = combobox.value;
+//------------------------------------------------------------------
+// Implement DOM
+
+document.getElementById("combobox").addEventListener("click", function () {
+    var value = document.getElementById("combobox");
     if (value == "rectangle") {
-        label2.style.display = "inline";
-        textbox2.style.display = "inline";
-        label1.innerHTML = "Length";
-        perimeter.innerHTML = "Calculate Perimeter";
+        document.getElementById("label2").style.display = "inline";
+        document.getElementById("textbox2").style.display = "inline";
+        document.getElementById("label1").innerHTML = "Length";
+        document.getElementById("perimeter").innerHTML = "Calculate Perimeter";
     } else if (value == "square") {
-        label1.innerHTML = "Side";
-        label2.style.display = "none";
-        textbox2.style.display = "none";
-        perimeter.innerHTML = "Calculate Perimeter";
+        document.getElementById("label1").innerHTML = "Side";
+        document.getElementById("label2").style.display = "none";
+        document.getElementById("textbox2").style.display = "none";
+        document.getElementById("perimeter").innerHTML = "Calculate Perimeter";
     } else if (value == "circle") {
-        label1.innerHTML = "radius";
-        label2.style.display = "none";
-        textbox2.style.display = "none";
-        perimeter.innerHTML = "Calculate Perimeter";
+        document.getElementById("label1").innerHTML = "Radius";
+        document.getElementById("label2").style.display = "none";
+        document.getElementById("textbox2").style.display = "none";
+        document.getElementById("perimeter").innerHTML = "Calculate Perimeter";
     } else if (value == "cube") {
-        label1.innerHTML = "Side";
-        label2.style.display = "none";
-        textbox2.style.display = "none";
-        perimeter.innerHTML = "Calculate Volume";
+        document.getElementById("label1").innerHTML = "Side";
+        document.getElementById("label2").style.display = "none";
+        document.getElementById("textbox2").style.display = "none";
+        document.getElementById("perimeter").innerHTML = "Calculate Volume";
     } else if (value == "cylinder") {
-        label2.style.display = "inline";
-        textbox2.style.display = "inline";
-        label1.innerHTML = "Radius";
-        label2.innerHTML = "Height";
-        perimeter.innerHTML = "Calculate Volume";
-    } else {
-        alert("Choose the shape first!");
+        document.getElementById("label2").style.display = "inline";
+        document.getElementById("textbox2").style.display = "inline";
+        document.getElementById("label1").innerHTML = "Radius";
+        document.getElementById("label2").innerHTML = "Height";
+        document.getElementById("perimeter").innerHTML = "Calculate Volume";
     }
 });
 
-area.addEventListener("click", function () {
-    var value = combobox.value;
+document.getElementById("area").addEventListener("click", function () {
+    var value = document.getElementById("combobox").value;
     if (value == "rectangle") {
-        length = textbox1.value;
-        width = textbox2.value;
+        length = document.getElementById("textbox1").value;
+        width = document.getElementById("textbox2").value;
         total = rectangleArea(length, width);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "square") {
-        side = textbox1.value;
+        side = document.getElementById("textbox1").value;
         total = squareArea(side);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "circle") {
-        radius = textbox1.value;
+        radius = document.getElementById("textbox1").value;
         total = circleArea(phi, radius);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "cube") {
-        side = textbox1.value;
+        side = document.getElementById("textbox1").value;
         total = cubeArea(side);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "cylinder") {
-        radius = textbox1.value;
-        height = textbox2.value;
+        radius = document.getElementById("textbox1").value;
+        height = document.getElementById("textbox2").value;
         total = cylinderArea(phi, radius, height);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else {
         alert("Choose the shape first!");
     }
 });
 
-perimeter.addEventListener("click", function () {
-    var value = combobox.value;
+document.getElementById("perimeter").addEventListener("click", function () {
+    var value = document.getElementById("combobox").value;
     if (value == "rectangle") {
-        length = textbox1.value;
-        width = textbox2.value;
+        length = document.getElementById("textbox1").value;
+        width = document.getElementById("textbox2").value;
         total = rectanglePerimeter(length, width);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "square") {
-        side = textbox1.value;
+        side = document.getElementById("textbox1").value;
         total = squarePerimeter(side);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "circle") {
-        radius = textbox1.value;
+        radius = document.getElementById("textbox1").value;
         total = circleCircumference(phi, radius);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "cube") {
-        side = textbox1.value;
+        side = document.getElementById("textbox1").value;
         total = cubeVolume(side);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else if (value == "cylinder") {
-        radius = textbox1.value;
-        height = textbox2.value;
+        radius = document.getElementById("textbox1").value;
+        height = document.getElementById("textbox2").value;
         total = cylinderVolume(phi, radius, height);
-        answer.value = total;
+        document.getElementById("answer").value = total;
     } else {
         alert("Choose the shape first!")
     }
